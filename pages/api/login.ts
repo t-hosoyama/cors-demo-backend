51fd77b1c6c751d84bfd739009724631c516cd0b
t-hosoyama <t-hosoyama@stylagy.co.jp>
2021-09-17 23:17:06 +0900

    add(feat): add dummy user auth

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -14,21 +14,30 @@ export default async function handler(
 ) {
   await nextCors(req, res, corsOptions)
 
-  console.log(req.cookies);
+  console.log('body:', req.body);
+  console.log('cookie:', req.cookies);
 
-  // 本来は ID と Pass で認証
-  const isLogin = true;
+  // ID と Pass で認証
+  const user = {
+    id: 'demo',
+    pass: 'test',
+  }
+  const isLogin = req.body.id === user.id && req.body.pass === user.pass;
 
   if (req.headers.origin === corsOptions.origin && isLogin) {
-    res.setHeader("Set-Cookie", [
+    res.setHeader('Set-Cookie', [
       `sid=${getSessionId()}`,
       'Domain=127.0.0.1',
       'Path=/',
+      `Max-Age=${60 * 60}`,
       'HttpOnly',
       'Secure',
       'SameSite=None'
     ].join(';'))
     res.json({ login: true, code: 200, message: 'sucess' })
+  } else if(!isLogin) {
+    // Cookieは削除？
+    res.json({ login: false, code: 401, message: 'Unauthorized' })
   } else {
     // Cookieは削除？
     res.json({ login: false, code: 403, message: 'Forbidden' })
